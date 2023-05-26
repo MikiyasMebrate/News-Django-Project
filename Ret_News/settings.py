37b566296ace09efd5fbb8f7bb39e7c1fd0ebeca
Mikiyas Mebrate <80261020+MikiyasMebrate@users.noreply.github.com>
2023-05-26 06:08:23 +0300

    Update settings.py

--- Ret_News/settings.py
+++ Ret_News/settings.py
@@ -136,8 +136,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mikiyasmebrate2656@gmail.com'
-EMAIL_HOST_PASSWORD ='bvkwnifwmbwzngfn' 
+EMAIL_HOST_USER = 'YOUR GOOGLE EMAIL'
+EMAIL_HOST_PASSWORD ='YOUR PASSWORD' 
 
 
 #
