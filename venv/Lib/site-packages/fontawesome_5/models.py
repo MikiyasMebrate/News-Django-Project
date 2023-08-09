8339b2c78dcc7ea76b05ba17031fe473ceaa9f35
Mikiyas Mebrate <80261020+MikiyasMebrate@users.noreply.github.com>
2023-08-09 08:16:42 +0300

    Delete venv directory

--- venv/Lib/site-packages/fontawesome_5/models.py
+++
@@ -1,8 +0,0 @@
-
-try:
-    from south.modelsinspector import add_introspection_rules
-    add_introspection_rules([], [
-        "^fontawesome_5\.fields\.IconField" # pylint: disable=anomalous-backslash-in-string
-    ])
-except ImportError:
-    pass
